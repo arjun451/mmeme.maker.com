b3c73a41ea81df3ec60817e65885363e61741509
arjun kumar singh <aksingh25071998@gmail.com>
2024-03-08 11:31:57 +0530

    draw a persion

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const ctx = canvas.getContext("2d")
 
 canvas.width = 800
 canvas.height= 600
- 
-ctx.fillRect(100,200,100,300)
-ctx.fillRect(400,200,100,300)
-ctx.fillRect(250,300,100,150)
-ctx.moveTo(100,200)
-ctx.lineTo(300,20)
-ctx.moveTo(300,20)
-ctx.stroke()
-ctx.lineTo(500,200)
-ctx.moveTo(500,200)
-ctx.lineTo(100,200)
-ctx.stroke()
-ctx.fillStyle()
+ctx.fillRect(350-5,230-5,20,80) 
+ctx.fillRect(400-5,200-5,50,200)
+ctx.fillRect(480-5,230-5,20,80)
+
+ctx.arc(420,130,50,0,Math.PI*2)
+ctx.fill()
+ctx.beginPath()
+ctx.fillStyle = "red"
+ctx.arc(400,109,5,Math.PI,Math.PI*2)
+ctx.arc(450,109,5,Math.PI,Math.PI*2)
+ctx.fill()
+ctx.beginPath()
+ctx.arc(420,140,15,0,Math.PI)
+ctx.fill()
